2c39ccbe75a13c48766da690f4280a09a7964856
Manuel Guariglia <manuelguariglia@gmail.com>
2021-12-14 22:37:11 +0100

    fix posts and sidebar

--- src/components/posts.vue
+++ src/components/posts.vue
@@ -10,7 +10,7 @@
           <img src="../assets/img/blog-54.jpg" alt="">
           <div class="post-body">
             <div class="row">
-              <div class="col-2">
+              <div class="col-1">
                 <div class="date">
                   <div class="day">
                     12
@@ -20,30 +20,38 @@
                   </div>
                 </div>
               </div>
-              <div class="col-10">
+              <div class="col-11">
                 <span class="post-title">How to Make Friends as a Grown-Up</span>
                 <p class="post-desc">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem quidem obcaecati molestiae alias quo quos temporibus atque facere accusantium! Iusto facilis nesciunt unde id illo sit laborum, impedit ab optio reprehenderit molestiae maxime ipsam deleniti, quae consequatur dignissimos nobis, voluptate earum asperiores tempore obcaecati ex architecto enim. Quae, voluptatibus dolore?
                 </p>
                 <div class="info-box">
-                  <div class="author">
-                    <i class="far fa-user"></i>
-                    <span>
-                      By <a href="#">Jhon Doe</a>
-                    </span>
+
+                  <div class="left-side">
+                    <div class="author">
+                      <i class="far fa-user"></i>
+                      <span>
+                        By <a href="#">Jhon Doe</a>
+                      </span>
+                    </div>
+                    <div class="category">
+                      <i class="far fa-folder"></i>
+                      <span>
+                        <a href="#">Lifestyle, Travel</a>
+                      </span>
+                    </div>
+                    <div class="comments">
+                      <i class="far fa-comments"></i>
+                      <span>
+                        <a href="#">12 Comments</a>
+                      </span>
+                    </div>
+                  </div>  
+
+                  <div class="right-side">
+                    <button type="button" class="btn btn-light">Read More</button>
                   </div>
-                  <div class="category">
-                    <i class="far fa-folder"></i>
-                    <span>
-                      <a href="#">Lifestyle, Travel</a>
-                    </span>
-                  </div>
-                  <div class="comments">
-                    <i class="far fa-comments"></i>
-                    <span>
-                      <a href="#">12 Comments</a>
-                    </span>
-                  </div>
+                  
                 </div>
                 <div class="line"></div>
               </div>
@@ -51,19 +59,78 @@
           </div>
         </div>
         <!-- /post -->
+        <!-- post -->
+        <div class="post">
+          <img src="../assets/img/blog-54.jpg" alt="">
+          <div class="post-body">
+            <div class="row">
+              <div class="col-1">
+                <div class="date">
+                  <div class="day">
+                    12
+                  </div>
+                  <div class="mounth">
+                    Jan
+                  </div>
+                </div>
+              </div>
+              <div class="col-11">
+                <span class="post-title">How to Make Friends as a Grown-Up</span>
+                <p class="post-desc">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem quidem obcaecati molestiae alias quo quos temporibus atque facere accusantium! Iusto facilis nesciunt unde id illo sit laborum, impedit ab optio reprehenderit molestiae maxime ipsam deleniti, quae consequatur dignissimos nobis, voluptate earum asperiores tempore obcaecati ex architecto enim. Quae, voluptatibus dolore?
+                </p>
+                <div class="info-box">
+
+                  <div class="left-side">
+                    <div class="author">
+                      <i class="far fa-user"></i>
+                      <span>
+                        By <a href="#">Jhon Doe</a>
+                      </span>
+                    </div>
+                    <div class="category">
+                      <i class="far fa-folder"></i>
+                      <span>
+                        <a href="#">Lifestyle, Travel</a>
+                      </span>
+                    </div>
+                    <div class="comments">
+                      <i class="far fa-comments"></i>
+                      <span>
+                        <a href="#">12 Comments</a>
+                      </span>
+                    </div>
+                  </div>  
+
+                  <div class="right-side">
+                    <button type="button" class="btn btn-light">Read More</button>
+                  </div>
+                  
+                </div>
+                <div class="line"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /post -->
+
       </div>
         
       <div class="col-4">
-      
-    </div>
+      <sidebar />
+      </div>
     </div>
   </div>
 </section>
 </template>
 
 <script>
+import sidebar from './sidebar.vue';
 export default {
   name: 'postGallery',
+  components: {
+   sidebar,
+  }
 }
 </script>
 
@@ -73,16 +140,17 @@ export default {
   .posts{
     margin-top: 50px;
     .post{
+      margin-bottom: 50px;
       img{
         width: 100%;
       }
       .post-body{
         margin-top: 20px;
         .date{
-          width: 50%;
           text-align: center;
           font-size: 12px;
           font-weight: bolder;
+          width: 100%;
           .day{
             background-color: #F7F7F7;
             color: black;
@@ -98,24 +166,39 @@ export default {
           color: black;
           font-weight: bolder;
           line-height: 50px;
+          font-size: 16px;
         }
         .post-desc{
           color: black;
-          font-size: 14px;
           line-height: 20px;
         }
         .info-box{
-          display: flex;
-          color: #888888;
-          span{
-            margin-right: 10px;
-            a{
-              color: $main-color;
-              text-decoration: none;
+            color: #888888;
+            font-size: 14px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 30px;
+          .left-side{
+            display: flex;
+            span{
+              margin-right: 10px;
+              a{
+                color: $main-color;
+                text-decoration: none;
+              }
+            }
+            i{
+              margin-right: 5px;
             }
           }
-          i{
-            margin-right: 5px;
+          .right-side{
+            button{
+              color: #888888;
+              padding: 2px;
+              text-transform: uppercase;
+              font-size: 12px;
+            }
           }
         }
       }
---
+++ src/components/sidebar.vue
@@ -0,0 +1,88 @@
+<template>
+  <section>
+
+    <div class="last-twitter">
+      <h6>latest from twitter</h6>
+
+      <div class="tweet">
+        <i class="fab fa-twitter"></i>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          <span class="tweet-date">
+          01:05 PM Sep 18th
+          </span>  
+        </p>
+      </div>
+
+      <div class="tweet">
+        <i class="fab fa-twitter"></i>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          <span class="tweet-date">
+          01:05 PM Sep 18th
+          </span>  
+        </p>
+      </div>
+      
+    </div>
+
+    <div class="instagram-photos">
+
+      <h6>photos from instagram</h6>
+
+      <div class="ig-grid">
+        <img src="../assets/img/29739607_2020680068220520_4509928046932787200_n.jpg" alt="">
+        <img src="../assets/img/30087804_253872848488989_8792603541668626432_n.jpg" alt="">
+        <img src="../assets/img/29415620_196477127626244_3250318472361541632_n.jpg" alt="">
+        <img src="../assets/img/30078414_1274410412703843_8290935809419247616_n.jpg" alt="">
+        <img src="../assets/img/30077195_2066750973610181_3733150062893596672_n.jpg" alt="">
+        <img src="../assets/img/29415304_166583630713703_1032667922171953152_n.jpg" alt="">
+      </div>
+
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+}
+</script>
+
+<style lang="scss">
+@import "../assets/style/vars.scss";
+@import "../assets/style/generals.scss";
+section{
+  .last-twitter{
+    .tweet{
+      display: flex;
+      i{
+        font-size: 25px;
+        color: black;
+        margin-right: 15px;
+      }
+      p{
+        color: #888888;
+        .tweet-date{
+          color: $main-color;
+          display: block;
+        }
+      }
+    }
+  }
+
+  .ig-grid{
+    display: flex;
+    flex-wrap: wrap;
+    img{
+      width: calc(100% / 3);
+    }
+  }
+  h6{
+    font-weight: bolder;
+    color: black;
+    text-transform: uppercase;
+  }
+}
+</style>
